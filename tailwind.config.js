4b32e82ca6d0e889a8277647d53aa1a5c8e30c99
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-10-12 00:50:51 -0400

    base-popup-and-configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+		colors: {
+			rf_yellow: "#eab01f",
+			rf_red: "#b4161c",
+			rf_black: "#131313",
+		},
 		extend: {},
 	},
 	plugins: [],
